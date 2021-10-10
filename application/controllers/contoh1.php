a84fbe9e28bc6d3b5d50ad969a7e34c7fab94ee5
tialistians <tialistians2000@gmail.com>
2021-10-10 19:39:58 +0700

    Web Programming II

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Tia Listia Nur Saadah
+            Saya tinggal di Kalideres
+            olahraga yang saya sukai adalah bulutangkis";
+    }
+}
